2f7449b0e6a7e19277e50c13b7888d2b7e66bbca
Alan Smith <commit@alanwsmith.com>
2022-11-14 14:33:21 -0600

    Clean up for deploy

--- src/extension.js
+++ src/extension.js
@@ -7,75 +7,14 @@
 
 ////////////////////////////////////////////////////////////
 //
-// OVERVIEW
-//
-// This is a Google Chrome extension designed to make
-// it a little easier to follow folks on Mastodon. It
-// captures your account name when you type it into
-// one of the "Proceed To Follow" pop-ups and then
-// fills it in automatically on other instances
-// that use the same format.
-//
-// This version is a prototype. It works on my
-// machine, but I haven't done extensive testing
-// elsewhere.
-//
-//
-
-////////////////////////////////////////////////////////////
-//
-// INSTALLATION
-//
-// I don't have the spoons to deploy this to the
-// chrome extensions store. You'll need to install
-// it manually. There's a few steps, but it's not
-// too bad. (Note: this involves turing on
-// "Developer mode". It's a direct feature that
-// Chrome provides but there can be security
-// implcaitons for it beyond what I have the
-// expertise to speak about. Best I can tell you
-// is make sure you trust any extenisons you install
-// that require it.)
-//
-//
-// 1. Download the `manifest.json` and `extension.js`
-//    files into a new folder somewhere convenient
-//    but out of the way on your computer. You'll
-//    pretty much only need to worry about the files
-//    and folder going through this install process.
-//    So, maybe don't put the folder on your desktop
-//    unless you want to see it there all the time.
-//
-// 2. Open Chrome's Prefernces/Settings and
-//    click into the "Extensions" page.
-//
-// 3. Toggle the "Developer mode" switch in the
-//    upper portion of the page.
-//
-// 4. Click the "Load unpaced" button which shows
-//    up when you turn on "Developer mode".
-//
-// 5. Navigate to the folder what you put the
-//    "manifest.json" and "extension.js" files and
-//    select it to complete the process.
-//
-//
-// This code is open source. Please feel free to
-// role it into an extension and put it in the store
-// for easier use if you'd like.
-
-////////////////////////////////////////////////////////////
-//
-// DETAILS
-//
-// This setion is for the "Proceed to Follow" pop-up
+// This section is for the "Proceed to Follow" pop-up
 // windows. The extension looks for the input field
 // and stores the value you type in there. When you
-// hit another popup, it automatially fills in the
+// hit another popup, it automatically fills in the
 // value.
 //
-// The process is relatively naive. It explictly
-// target an element with the id 'remove_follow_acct'
+// The process is relatively naive. It explicitly
+// target an element with the id 'remtve_follow_acct'
 // and work on it. That's been the name every time
 // I've seen a pop-up, but I imagine there's
 // customizations that change that.
@@ -84,122 +23,118 @@
 // site that has a field with that ID will get updated.
 // The means any site you visit can get whatever you
 // most recently typed in.
-//
 
 ////////////////////////////////////////////////////////////
 
-// A thing to hold the id
-
 let mastodonUserId = null
 
-// Function to store the account name typed
-// into a remote follow box for later use
-
+// Store the account string for later
 const handleTextChange = (event) => {
   if (event.target.value !== mastodonUserId) {
     mastodonUserId = event.target.value
-    chrome.storage.sync.set({ mastodonUserId: mastodonUserId }, function () {
-      // console.log(`mastodonUserId is now: ${mastodonUserId}`)
-    })
+    chrome.storage.sync.set({ mastodonUserId: mastodonUserId })
   }
 }
 
-// Make a grab for the form field for
-// the asks you for your accound id when
-// you try to follow someone
-
+// Grab for the field
 const theFormField = document.getElementById('remote_follow_acct')
 
-// If you find it, stack the event listener
-// on it to watch for changes and fill it
-// in with an existing account ID if it's set.
-// From there, you can manually click the
-// follow button to complete the process
-
+// Process the field if you find it
 if (theFormField) {
   theFormField.addEventListener('input', handleTextChange)
   chrome.storage.sync.get(['mastodonUserId'], function (result) {
     mastodonUserId = result.mastodonUserId
     theFormField.value = mastodonUserId
-    // console.log(`Loaded mastodonUserId: ${mastodonUserId}`)
   })
 }
 
 ////////////////////////////////////////////////////////////
 //
-// THIS IS NOT YET IMPLEMENTED
+// Phase 2 - DOES NOT WORK YET. The goal of this section
+// is to automatically redirect you to your account and
+// fill in the search field to find someone you want to
+// follow via that structure (this is when you get a
+// modal dialog box instead of the distinct pop-up window)
 //
-// This section is for handling the case for when you hit
-// a follow button and a modal pops up. The behavoir in this
-// case it to grab the url of the use you just clicked the
-// follow button on and then open a new browser tab with your
-// home page and paste the URL into the search field. From
-// there can type enter to do the search.
+// I can capture the persons url and redirect to your
+// account page and fill in the search, but I haven't figured
+// out how to do the search yet. If you know, please show
+// me how. I'd love to add that as a feature.
 //
-// I haven't been able to figuore out how to make that
-// happen automatically yet. If you know the secret sauce,
-// hit me up: https://hachyderm.io/web/@TheIdOfAlan
+// I'm commenting this all out for now.
+//
+////////////////////////////////////////////////////////////
+//
 
+// let linkIsShowing = false
 // const watchForMastodonFollowBox = () => {
-//   // Give it a half second to load
+//   // Give it a moment to load after catching the click
 //   setTimeout(() => {
-//     const mastodonUserToFollowInputBox = document.body.querySelector(`
+//     const mastodonUserToFollowInputBoxArea = document.body.querySelector(`
 //       div.interaction-modal__choices__choice >
-//       div.copypaste >
-//       input[type=text]
+//       div.copypaste
 //     `)
-//     console.log(mastodonUserToFollowInputBox)
-//     if (mastodonUserToFollowInputBox) {
-//       const mastodonUserToFollowUrl = mastodonUserToFollowInputBox.value
-//       console.log(mastodonUserToFollowUrl)
-//       if (mastodonUserToFollowUrl.indexOf('@') > -1) {
-//         chrome.storage.sync.set(
-//           { mastodonUserToFollowUrl: mastodonUserToFollowUrl },
-//           function () {
-//             window.open('https://hachyderm.io/web/home', '_blank')
-//             console.log(
-//               `mastodonUserToFollowUrl is now: ${mastodonUserToFollowUrl}`
-//             )
-//           }
-//         )
-//       }
-//       // console.log(searchUserUrlString)
+//     if (mastodonUserToFollowInputBoxArea) {
+//       chrome.storage.sync.get(['mastodonUserAccountUrl'], function (result) {
+//         if (result.mastodonUserAccountUrl && linkIsShowing === false) {
+//           linkIsShowing = true
+//           const newEl = document.createElement('a')
+//           newEl.href = result.mastodonUserAccountUrl
+//           newEl.text = 'Click here to go to your account'
+//           mastodonUserToFollowInputBoxArea.appendChild(newEl)
+//         }
+//       })
 //     }
 //   }, 300)
-//   console.log('hhhhhhhhhhhhhhhhhhhhhhhhhhh')
 // }
 // document.body.addEventListener('click', watchForMastodonFollowBox)
-// console.log('eeeeeeeeeeeeee')
 
-// See if you're on a mastodon pages that has the same
-// setup as https://hachyderm.io/
+//// See if you're on a mastodon pages that has the same
+//// setup as https://hachyderm.io/
+//// Look for a how page search field.
+//const homePageSearchField = document.querySelector(
+//  'div#mastodon input.search__input'
+//)
+//if (homePageSearchField) {
+//  console.log(homePageSearchField)
+//  // Store the URL so you can send to it later.
+//  // There's probably more logic that can be added
+//  // here to make sure you pick up the right thing.
+//  const mastodonUserAccountUrl = document.location.href
+//  chrome.storage.sync.set(
+//    { mastodonUserAccountUrl: mastodonUserAccountUrl },
+//    function () {
+//      console.log(`Stored: ${mastodonUserAccountUrl}`)
+//    }
+//  )
+//  ///////////////////////////
+//  //Testing
+//  //
+//  homePageSearchField.focus()
+//  setTimeout(() => {
+//    const event = new KeyboardEvent('keyup', {
+//      key: 'a',
+//      bubbles: true,
+//    })
+//    console.log(event)
+//  }, 1400)
+//}
 
-// const homePageSearchField = document.querySelector(
-//   'div#mastodon input.search__input'
-// )
-
-// if (homePageSearchField) {
-//   console.log(homePageSearchField)
-//   chrome.storage.sync.get(['mastodonUserToFollowUrl'], function (result) {
-//     if (result.mastodonUserToFollowUrl) {
-//       console.log(`-- FOUND URL: ${result.mastodonUserToFollowUrl}`)
-//       // homePageSearchField.focus()
-//       console.log('mmmmmmmmmmmmmmmmm')
-//       homePageSearchField.value = `${result.mastodonUserToFollowUrl}`
-//       // const ke = new KeyboardEvent('keyup', {
-//       //   bubbles: true,
-//       //   cancelable: true,
-//       //   keyCode: 13,
-//       // })
-//       // document.body.dispatchEvent(ke)
-//       dispatch(submitSearch())
-//       // homePageSearchField.submit()
-//     }
-//     // mastodonUserId = result.mastodonUserId
-//     // theFormField.value = mastodonUserId
-//     // console.log(`Loaded mastodonUserId: ${mastodonUserId}`)
-//   })
-// }
+//  // Now check to see if there's a url in the payload
+//  // and drop it in the search box if there is.
+//  ///////////// NOTE This may not work the same with with
+//  //advanced view and not advanced vie
+//  //
+//  chrome.storage.sync.get(['mastodonUserToFollowUrl'], function (result) {
+//    if (result.mastodonUserToFollowUrl) {
+//      homePageSearchField.focus()
+//      homePageSearchField.value = `${result.mastodonUserToFollowUrl}`
+//      chrome.storage.sync.remove(['mastodonUserToFollowUrl'], function () {
+//        console.log('Removing follow URL')
+//      })
+//    }
+//  })
+//}
 
 // const changeBackgroundForTesting = () => {
 //   const number = Math.floor(Math.random() * 16777215)
@@ -210,7 +145,3 @@ if (theFormField) {
 // document.body.addEventHandler('click', changeBackground)
 // document.body.style.backgroundColor = '#123434'
 // document.getElementById('remote_follow_acct').value = 'asdf'
-
-// document.addEventListener('DOMContentLoaded', initStuff)
-
-// console.log('ccccccccccccccccc')
